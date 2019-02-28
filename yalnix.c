afe70556fc0f19dd13ca14f1cc12a16630e44f12
Robert Rowland Foley <rrf2@amber.clear.rice.edu>
2019-02-28 16:42:57 -0600

    added code to create the free list - needs to be tested

--- yalnix.c
+++ yalnix.c
@@ -19,7 +19,7 @@ int virtual_memory;
 void *kernel_brk;
 
 void *interrupt_vector[TRAP_VECTOR_SIZE];
-struct pfn_list free_pfn_list = malloc(pmem_size  / PAGESIZE * sizeof(struct pfn));
+// struct pfn_list free_pfn_list = malloc(pmem_size  / PAGESIZE * sizeof(struct pfn));
 
 void KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, char **cmd_args);
 int SetKernelBrk(void *addr);
@@ -28,31 +28,47 @@ int SetKernelBrk(void *addr);
 
 void
 KernelStart(ExceptionInfo *info, unsigned int pmem_size, void *orig_brk, char **cmd_args) {
-	//TODO: initialize interrupt vector table
-
-	interruptVector[TRAP_KERNEL] = &trap_kernel_handler;
-	interruptVector[TRAP_CLOCK] = &trap_clock_handler;
-	interruptVector[TRAP_ILLEGAL] = &trap_illegal_handler; 
-	interruptVector[TRAP_MEMORY] = &trap_memory_handler;
-	interruptVector[TRAP_MATH] = &trap_math_handler;
-	interruptVector[TRAP_TTY_RECEIVE] = &trap_tty_receive_handler;
-	interruptVector[TRAP_TTY_TRANSMIT] = &trap_tty_transmit_handler;
-	
+	// KEEP TRACK OF THE KERNEL BRK and VIRTUAL MEMORY FLAG
 	kernel_brk = orig_brk;
+	virtual_memory = 0;
 
+	// CREATE INrERRUPT VECTOR
+	interruptVector[TRAP_KERNEL] = trap_kernel_handler;
+	interruptVector[TRAP_CLOCK] = trap_clock_handler;
+	interruptVector[TRAP_ILLEGAL] = trap_illegal_handler;
+	interruptVector[TRAP_MEMORY] = trap_memory_handler;
+	interruptVector[TRAP_MATH] = trap_math_handler;
+	interruptVector[TRAP_TTY_RECEIVE] = trap_tty_receive_handler;
+	interruptVector[TRAP_TTY_TRANSMIT] = trap_tty_transmit_handler;
 
-
-	// interrupt_vector = malloc(TRAP_VECTOR_SIZE * sizeof(void *))
-
+	// WRITE TO REG_VECTOR_BASE
 	WriteRegister(REG_VECTOR_BASE, (RCS421RegVal) interrupt_vector);
 
-	struct pfn_list free_pfn_list = malloc(pmem_size  / PAGESIZE * sizeof(struct pfn));
 
-	// USED PAGES -> &_etext down to page, and orig_brk up to page
+	// struct pfn_list free_pfn_list = malloc(pmem_size  / PAGESIZE * sizeof(struct pfn));
 
-	// struct page_table kernel_page_table = &_etext;
+	// DETERMINE ALREADY USED PAGES
+	struct pfn used_pages_min = DOWN_TO_PAGE(VMEM_1_BASE);
+	struct pfn used_pages_max = UP_TO_PAGE(orig_brk)
+	printf("%d", used_pages_min)
+	printf("%d", used_pages_max)
+	// USED PAGES -> VEM_BASE_1 down to page, and orig_brk up to page
 
-	virtual_memory = 0;
+	// CREATE THE HEAD OF THE FREE LIST
+	struct pfn_list_entry free_pfn_head;
+	free_pfn_head.pfn = 0;
+	free_pfn_head.next = NULL;
+
+	// CREATE THE FREE LIST
+	int i;
+	for (i = MEM_INVALID_PAGES; i < pmemsize / PAGESIZE; i++) {
+		if (i > used_pages_min || i < used_pages_max) {
+			struct pfn_list_entry entry;
+			entry.pfn = i;
+			entry.next = free_pfn_head;
+			free_pfn_head = entry;
+		}
+	}
 }
 
 int
